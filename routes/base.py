448c862fdbbda85a9fa87732370501c4feabda90
YousraElmag <yousra3489@gmail.com>
2026-04-19 15:45:11 +0200

    update project structure and dependencies

--- routes/base.py
+++ routes/base.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+import os
 base_router=APIRouter(
 prefix="/api/v1",
 tags=["api_v1"]
@@ -8,6 +9,10 @@ tags=["api_v1"]
 )
 @base_router.get("/")
 def welcome():
+    app_name=os.getenv("APP_NAME")
+    app_version=os.getenv("APP_VERSION")
     return{
-        "message":"Hello World"
+       "app_name":app_name,
+       "app_version":app_version
+       
     }
